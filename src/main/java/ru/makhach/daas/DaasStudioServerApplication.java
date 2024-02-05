d223c2f208a1fd579b6ae0eb2efeade91777c4fe
Abdulazizov Makhach <abdulazizov2001@mail.ru>
2024-02-05 05:46:26 +0300

    init commit

---
+++ src/main/java/ru/makhach/daas/DaasStudioServerApplication.java
@@ -0,0 +1,13 @@
+package ru.makhach.daas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaasStudioServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DaasStudioServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/makhach/daas/DaasStudioServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.makhach.daas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaasStudioServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
